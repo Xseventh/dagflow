d5c15a46dababe508e806d400b922bee1dbd3fbe
seventh <xuweiqi117@gmail.com>
2021-02-19 02:15:24 +0800

    add thread_pool to common

---
+++ common/lock_queue.h
@@ -0,0 +1,40 @@
+//
+// Created by seventh on 2020/12/6.
+//
+
+#ifndef DAGFLOW_COMMON_LOCK_QUEUE_H_
+#define DAGFLOW_COMMON_LOCK_QUEUE_H_
+#include <queue>
+#include <mutex>
+namespace dagflow {
+namespace common {
+
+template<typename T>
+class LockQueue {
+  public:
+    bool push(T const &value) {
+        std::lock_guard<std::mutex> lock(this->mutex);
+        this->q.push(value);
+        return true;
+    }
+    // deletes the retrieved element, do not use for non integral types
+    bool pop(T &v) {
+        std::lock_guard<std::mutex> lock(this->mutex);
+        if (this->q.empty())
+            return false;
+        v = this->q.front();
+        this->q.pop();
+        return true;
+    }
+    bool empty() {
+        std::lock_guard<std::mutex> lock(this->mutex);
+        return this->q.empty();
+    }
+  private:
+    std::queue<T> q;
+    std::mutex mutex;
+};
+
+}
+}
+#endif //DAGFLOW_COMMON_LOCK_QUEUE_H_
